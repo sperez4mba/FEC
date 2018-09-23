26d8ddee4245effbff121a7c1f4b49be3fd5fa41
Sergio Perez <sperez4mba@gmail.com>
2018-09-23 19:52:21 +0200

    Crea api routes para listar y detalle de jugadores

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Jugador;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,14 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('jugadores', function() {
+    // If the Content-Type and Accept headers are set to 'application/json', 
+    // this will return a JSON structure. This will be cleaned up later.
+    return Jugador::all();
+});
+
+Route::get('jugadores/{id}', function($id) {
+	    return Jugador::find($id);
+});
+
